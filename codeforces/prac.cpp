3f1d7ab9c970e7797b43100fa2866eeea99dbdbd
saarang123 <saarangsrini35@gmail.com>
2021-07-27 11:05:20 +0530

    new

--- codeforces/prac.cpp
+++ codeforces/prac.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ld = long double;
-#define int long long
+//#define int long long
 #define all(x) x.begin(), x.end()
 #define pb push_back
 #define mp make_pair
@@ -13,6 +13,167 @@ template<class T> bool remax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 template<typename T>
 istream& operator >> (istream& is, vector<T> &a) { for(T &element : a) is >> element; return is; }
 
+// Taken from neal(https://codeforces.com/profile/neal)
+template<const int MOD>
+struct _m_int {
+    int val;
+
+    _m_int(int64_t v = 0) {
+        if (v < 0) v = v % MOD + MOD;
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+
+    _m_int(uint64_t v) {
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+
+    _m_int(int v) : _m_int(int64_t(v)) {}
+    _m_int(unsigned v) : _m_int(uint64_t(v)) {}
+
+    static int inv_mod(int a, int m = MOD) {
+        // https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Example
+        int g = m, r = a, x = 0, y = 1;
+
+        while (r != 0) {
+            int q = g / r;
+            g %= r; swap(g, r);
+            x -= q * y; swap(x, y);
+        }
+
+        return x < 0 ? x + m : x;
+    }
+
+    explicit operator int() const { return val; }
+    explicit operator unsigned() const { return val; }
+    explicit operator int64_t() const { return val; }
+    explicit operator uint64_t() const { return val; }
+    explicit operator double() const { return val; }
+    explicit operator long double() const { return val; }
+
+    _m_int& operator+=(const _m_int &other) {
+        val -= MOD - other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    _m_int& operator-=(const _m_int &other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif
+        // Optimized mod for Codeforces 32-bit machines.
+        // x must be less than 2^32 * m for this to work, so that x / m fits in an unsigned 32-bit int.
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x);
+        unsigned quot, rem;
+        asm("divl %4\n"
+            : "=a" (quot), "=d" (rem)
+            : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    _m_int& operator*=(const _m_int &other) {
+        val = fast_mod(uint64_t(val) * other.val);
+        return *this;
+    }
+
+    _m_int& operator/=(const _m_int &other) {
+        return *this *= other.inv();
+    }
+
+    friend _m_int operator+(const _m_int &a, const _m_int &b) { return _m_int(a) += b; }
+    friend _m_int operator-(const _m_int &a, const _m_int &b) { return _m_int(a) -= b; }
+    friend _m_int operator*(const _m_int &a, const _m_int &b) { return _m_int(a) *= b; }
+    friend _m_int operator/(const _m_int &a, const _m_int &b) { return _m_int(a) /= b; }
+
+    _m_int& operator++() {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+
+    _m_int& operator--() {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+
+    _m_int operator++(int) { _m_int before = *this; ++*this; return before; }
+    _m_int operator--(int) { _m_int before = *this; --*this; return before; }
+
+    _m_int operator-() const {
+        return val == 0 ? 0 : MOD - val;
+    }
+
+    friend bool operator==(const _m_int &a, const _m_int &b) { return a.val == b.val; }
+    friend bool operator!=(const _m_int &a, const _m_int &b) { return a.val != b.val; }
+    friend bool operator<(const _m_int &a, const _m_int &b) { return a.val < b.val; }
+    friend bool operator>(const _m_int &a, const _m_int &b) { return a.val > b.val; }
+    friend bool operator<=(const _m_int &a, const _m_int &b) { return a.val <= b.val; }
+    friend bool operator>=(const _m_int &a, const _m_int &b) { return a.val >= b.val; }
+
+    _m_int inv() const {
+        return inv_mod(val);
+    }
+
+    _m_int pow(int64_t p) const {
+        if (p < 0)
+            return inv().pow(-p);
+
+        _m_int a = *this, result = 1;
+
+        while (p > 0) {
+            if (p & 1)
+                result *= a;
+
+            p >>= 1;
+
+            if (p > 0)
+                a *= a;
+        }
+
+        return result;
+    }
+
+    friend ostream& operator << (ostream &os, const _m_int &m) { return os << m.val; }
+    friend istream& operator >> (istream& is, _m_int &m) {is >> m.val; m.val %= MOD; return is;}
+};
+
+const int MOD = 1e9 + 7;
+using mint = _m_int<MOD>;
+
+//requires modint
+const int N = 1003;
+template<class T>
+T fexpo(T a, long long b) {
+    T res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a);
+        a = (a * a);
+        b >>= 1;
+    }
+    return res;
+}
+
+vector<mint> fact(N), inv(N);
+
+void init() {
+    fact[0] = inv[0] = mint(1);
+    for(int i = 1; i < N; i++) {
+        (fact[i] = fact[i - 1] * i);
+    }
+    for(int i = 1; i < N; i++)
+        inv[i] = fexpo(fact[i], MOD - 2);
+}
+
+mint nCr(int n, int r) {
+    return (r > n ? mint(0) : ((fact[n] * inv[r]) * inv[n - r]));
+}
 
 void solve_case();
 
@@ -28,45 +189,36 @@ signed main() {
     return 0;
 }
 
-const int MXN = 200 * 1000 + 3;
-vector<int> g[MXN];
-int dp[MXN], a[MXN], t[MXN];
-bool vis[MXN];
-int n, m;
-
-int dfs(int v, int p) {
-    if(vis[v])
-        return 0;
-    dp[v] = 0;
-    vis[v] = true;
-    int sum = a[v];
-    for(int u : g[v]) if(u != p) {
-        sum += dfs(u, v);
-    }
-    dp[v] = t[v] - sum;
-    return dp[v];
-}
-
 void solve_case() {
-    cin >> n >> m;
-    for(int i = 0; i <= n; i++) {
-        dp[i] = a[i] = t[i] = 0;
-        vis[i] = false;
-        g[i].clear();
-    }
-    for(int i = 1; i <= n; i++) 
-        cin >> a[i];
-    for(int i = 1; i <= n; i++) 
-        cin >> t[i];
-
-    for(int u, v, i = 0; i < m; i++) {
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> g(n);
+    for(int u, v, i = 1; i < n; i++) {
         cin >> u >> v;
+        u--, v--;
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    dfs(1, -1);
-    // for(int i = 1; i <= n; i++) 
-    //     cout << dp[i] << ' ';
-    // cout << '\n';
-    cout << (dp[1] ? "NO" : "YES") << '\n';
+    vector<int> cnt(n), dt(n);
+    function<void(int, int)> dfs = [&] (int v, int p) {
+        cnt[dt[v]]++;
+        for(int u : g[v]) if(u != p) {
+            dt[u] = dt[v] + 1;
+            dfs(u, v);
+        }
+    };
+    mint ans = 0;
+    for(int i = 0; i < n; i++) {
+        cout << "in: " << i << endl;
+        cnt = vector<int>(n, 0);
+        dt[i] = 0;
+        dfs(i, -1);
+        for(int j = 0; j < n; j++)
+            cout << cnt[j] << ' ';
+        cout << endl;
+        for(int j = 0; j < n; j++) 
+            if(cnt[j] >= k)
+                ans += nCr(cnt[j], k);
+    }
+    cout << ans << '\n';
 }
--- codeforces/pyprac.py
+++ codeforces/pyprac.py
@@ -1,96 +1,18 @@
-import pickle
+alpha = "abcdefghijklmnopqrstuvwxyz"
+ok = []
+for i in range(26):
+    ok.append(0)
+fib = [0, 1]
+for i in range(30):
+    fib.append(fib[-1] + fib[-2])
 
-def fill():
-    f = open('emp.dat', 'wb+')
-    data = {}
-    for _ in range(int(input('Enter number of entries: '))):
-        empno = input('Enter employee number: ')
-        data[empno] = []
-        data[empno].append(input('Enter employee name: '))
-        data[empno].append(int(input('Enter hourly wage rate: ')))
-        data[empno].append(int(input('Hours worked per week: ')))
-    print(data)
-    pickle.dump(data, f)
-    f.close()
+for i in range(26):
+    ok[i] = (i + fib[i]) % 26
+print(ok)
+s = "apollomission"
+ans = ""
+for i in range(len(s)):
+    cnt = ord(s[i]) - ord('a')
+    ans += alpha[ok[cnt]]
 
-def new_entry():
-    f = open('emp.dat', 'wb+')
-    data = pickle.load(f)
-    empno = input('Enter employee number: ')
-    data[empno] = []
-    data[empno].append(input('Enter employee name: '))
-    data[empno].append(int(input('Enter hourly wage rate: ')))
-    data[empno].append(int(input('Hours worked per week: ')))
-    f.seek(0, 0)
-    pickle.dump(data, f)
-    f.close()
-
-def delete():
-    x = input('Enter employee number to delete: ')
-    f = open('emp.dat', 'wb+')
-    data = pickle.load(f)
-    del data[x]
-    f.seek(0, 0)
-    pickle.dump(data, f)
-    f.close()
-
-def modify():
-    x = input('Enter employee number to modify: ')
-    val = int(input('Enter new hourly wage rate: '))
-    f = open('emp.dat', 'wb+')
-    data = pickle.load(f)
-    data[x][1] = val
-    f.seek(0, 0)
-    pickle.dump(data, f)
-    f.close()
-
-def calc(cost, hrs):
-    regular = cost * hrs
-    bonus = 30 * cost / 100 * max(0, hrs - 40)
-    total = regular + wage
-    return bonus, total
-
-def displaywage():
-    f = open('emp.dat', 'rb')
-    f.seek(0, 0)
-    data = pickle.load(f)
-    for no in data:
-        details = data[no]
-        bonus, total = calc(details[1], details[2])
-        print("Employee:", no, "has bonus and total wage of", bonus, total)
-    f.close()
-
-def table():
-    f = open('emp.dat', 'rb')
-    data = pickle.load(f)
-    print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format("Emp no", "Name", "Wage Rate", "Hours", "Bonus", "Total Wage"))
-    for no in data:
-        details = data[no]
-        bonus, total = calc(details[1], details[2])
-        print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format(no, details[0], details[1], details[2], bonus, total))
-    f.close()
-
-menu = '''1.Enter the records of the file
-2.Display wages of each employee
-3.Display the file in a tabular format
-4.Delete a record from the file
-5.Modify a record of the file
-6.Append a record to the file
-0.exit'''
-c = 100
-while c > 0:
-    print(menu)
-    c = int(input('Enter choice: '))
-    if c == 1: 
-        fill()
-    elif c == 2: 
-        displaywage()
-    elif c == 3: 
-        table()
-    elif c == 4: 
-        delete()
-    elif c == 5:
-        modify()
-    elif c == 6:
-        new_entry()
-
+print(ans)
